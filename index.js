bb37340c6ab020800801ca3f25979157afcc6ecc
TGV <tgv@DESKTOP-LR1SC37.localdomain>
2019-07-28 08:56:37 +0800

    Test for args first

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ var fs = require('fs');
 let encode = require('@prudent/encode');
 
 exports.start = function (callback) {
-
-    process.stdin.setEncoding('utf8');
-    process.stdin.on('readable', () => {
-
-        try {
-            const input = process.stdin.read();
-            if (input !== null) {
-                let object = JSON.parse(input);
-                if (object.name === 'file') {
-                    callback(fs.readFileSync(object.content, 'utf8'));
-                }
-            } else if (process.argv[2] !== null) {
-                callback(fs.readFileSync(process.argv[2], 'utf8'));
-            }
-        } catch (e) {
-            console.log(encode.protocol('error', e.message));
+    try {
+        if (process.argv[2] !== undefined) {
+            callback(fs.readFileSync(process.argv[2], 'utf8'));
+        } else {
+            process.stdin.setEncoding('utf8');
+            process.stdin.on('readable', () => {
+                    const input = process.stdin.read();
+                    if (input !== null) {
+                        let object = JSON.parse(input);
+                        if (object.name === 'file') {
+                            callback(fs.readFileSync(object.content, 'utf8'));
+                        }
+                    }
+            });    
         }
-
-    });
-    
+    } catch (e) {
+        console.log(encode.protocol('error', e.message));
+    }
 }
